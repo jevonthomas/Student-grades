89bc29f5bf655cf679024e915b5b939441bb5c8c
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-27 13:13:46 -0500

    set up initial exercise

---
+++ grades.js
@@ -0,0 +1,13 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+// A score of 50-60 is an F
+// A score of 61-70 is a D
+// A score of 71-80 is a C
+// A score of 81-90 is a B
+// A score of 91-100 is an A
+
+for (var i = 0; i < scores.length; i++) {
+	if (i >= 50 || i <= 60) {
+		var 
+	}
+}
